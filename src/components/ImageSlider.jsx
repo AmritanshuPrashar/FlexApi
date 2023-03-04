828912abf645519c547c4aa4c2e85e23d01b3bc5
Amritanshu <amritanshu1002@gmail.com>
2023-03-04 18:30:16 +0530

    Few Bug fixes

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,50 +1,39 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
-import "./assets/imageSl.css"
+import "./assets/imageSl.css";
 import Images from "./Images";
 function ImageSlider() {
-	const [images, setImages] = useState([])
-	const options = {
-		method: 'GET',
-		url: 'https://premium-anime-mobile-wallpapers-illustrations.p.rapidapi.com/rapidHandler/anime',
-		params: {page: '0', sensitivity: '0', quality: '1'},
-		headers: {
-		  'X-RapidAPI-Key': 'd9e72d5342msha5223a25ee12e1fp176a8djsn77e13318c0fa',
-		  'X-RapidAPI-Host': 'premium-anime-mobile-wallpapers-illustrations.p.rapidapi.com'
-		}
-	  };
+	const [images, setImages] = useState([]);
+	
 	useEffect(() => {
-		axios.request(options).then(function (response) {
-			setImages(response.data);
-		}).catch(function (error) {
-			console.error(error);
-		});
-	
+		axios.get('https://api.unsplash.com/photos?client_id=JsC7huJJRSLGROk9YhdYAAbzVdAPt4PxP1WUlmwZQAs')
+			.then((res) => {
+				console.log(res.data)
+				setImages(res.data)
+			})
+			.catch(function (error) {
+				console.error(error);
+			});
+	}, [setImages]);
 
-	}, [])
-	
 	return (
 		<Carousel fade className="carousel-main">
-			{
-				images.map((image) => {
-					return (
-						<Carousel.Item >
+			{images.map((image) => {
+				return (
+					<Carousel.Item key={image.id}>
 						<img
-							className="d-block w-100 align-items-center"
-							src={image.arturl}
+							className="d-block h-500 w-100 align-items-center"
+							src={image.urls.regular}
 							alt="First slide"
 						/>
 						<Carousel.Caption>
-								<h3>{image.animename}</h3>
+							
 							<p></p>
 						</Carousel.Caption>
 					</Carousel.Item>
-					)
-				})
-				
-
-			}
+				);
+			})}
 		</Carousel>
 	);
 }
